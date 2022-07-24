e3b718e895cfa5c3e9e1e9046e5dbdadc49474bd
Rodrigo <rodrigofndev@gmail.com>
2022-07-24 09:45:14 -0300

    Inserindo spinner

--- src/app/utils/loading-spinner/loading-spinner.tsx
+++ src/app/utils/loading-spinner/loading-spinner.tsx
@@ -1,9 +1,16 @@
+import Spinner from 'react-bootstrap/esm/Spinner'
 import './loading-spinner.scss'
 
 const LoadingSpinner = () => {
   // const [isDataLoading, setDataLoading] = useState(true)
 
-  const loadingSpinner = <div className="block-page"></div>
+  const loadingSpinner = (
+    <div className="block-page d-flex align-items-center justify-content-center">
+      <Spinner animation="grow" role="status" className="spinner">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
+    </div>
+  )
 
   return true ? loadingSpinner : <></>
 }
