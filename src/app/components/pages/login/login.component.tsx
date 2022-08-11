0f143c88d2a4398ccae7ec7bc22d6f5ce09bb9bc
Rodrigo <rodrigofndev@gmail.com>
2022-08-11 17:23:02 -0300

    Criado botao e ajeitado estilo da pagina inicial

--- src/app/components/pages/login/login.component.tsx
+++ src/app/components/pages/login/login.component.tsx
@@ -1,73 +1,75 @@
 import './login.scss'
-// import logo from '../../../assets/ANOTHER LAND ENTERTAINMENT_LOGO D.png'
-// import TextInput from '../../../shared/components/input/text-input/text-input.component'
-// import { AiOutlineUser } from 'react-icons/ai'
-import { FieldValues, useForm } from 'react-hook-form'
-import { useAppDispatch } from '../../../store/hooks'
-import { UserLogin } from '../../../model/auth/auth.models'
-import { AuthAction } from '../../../store/auth-state/auth.reducer'
-import { Button } from 'react-bootstrap'
+import logo from '../../../assets/img/another-land-logo-with-name.png'
+// import al from '../../../assets/img/al-icon.png'
+import TextInput from '../../../shared/components/input/text-input/text-input.component'
+import { AiOutlineUser } from 'react-icons/ai'
+import { RiLockPasswordFill } from 'react-icons/ri'
+// import { FieldValues, useForm } from 'react-hook-form'
+// import { useAppDispatch } from '../../../store/hooks'
+// import { UserLogin } from '../../../model/auth/auth.models'
+// import { AuthAction } from '../../../store/auth-state/auth.reducer'
+import { Button, Col, Container, Row } from 'react-bootstrap'
 function LoginComponent() {
-  const dispatch = useAppDispatch()
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm()
+  // const dispatch = useAppDispatch()
+  // const {
+  //   register,
+  //   handleSubmit,
+  //   formState: { errors },
+  // } = useForm()
 
-  const onSubmit = (data: FieldValues) => {
-    const userData = data as UserLogin
-    dispatch(AuthAction.loginWithPassword(userData))
-  }
+  // const onSubmit = (data: FieldValues) => {
+  //   const userData = data as UserLogin
+  //   dispatch(AuthAction.loginWithPassword(userData))
+  // }
   console.log('Login')
   return (
-    // <Container
-    //   fluid
-    //   className="login-view d-flex flex-column align-items-center justify-content-center"
-    // >
-    //   <Row className="bg-warning w-100 text-center">
-    //     <Col sm={12}>
-    //       <picture>
-    //         <source
-    //           srcSet={'img_smallflower.jpg'}
-    //           media={'(max-width: 400px)'}
-    //         />
-    //         <source srcSet={logo} />
-    //         <img src={logo} alt={'Another land logo'} className="logo" />
-    //       </picture>
-    //     </Col>
-    //   </Row>
-    //   <Row className="bg-danger w-100 text-center">
-    //     <Col className="" sm={3}>
-    //       <TextInput icon={<AiOutlineUser />} />
-    //     </Col>
-    //     <Col className="" sm={12}>
-    //       Teste
-    //     </Col>
-    //     <Col className="" sm={12}>
-    //       Teste
-    //     </Col>
-    //   </Row>
-    // </Container>
-    <div className="grid-container">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input
-          className="border"
-          {...register('userName', { required: true })}
-        />
-        User
-        <hr />
-        <input
-          className="border"
-          {...register('password', { required: true })}
-        />
-        Password
-        <hr />
-        <Button type={'submit'}>Login</Button>
-        <hr />
-        {errors.exampleRequired && <span>This field is required</span>}
-      </form>
-    </div>
+    <Container
+      fluid
+      className="login-view d-flex flex-column align-items-center justify-content-center al-login"
+    >
+      <Row className="w-100 text-center">
+        <Col sm={12}>
+          <picture>
+            <source srcSet={logo} />
+            <img src={logo} alt={'Another land logo'} className="logo" />
+          </picture>
+        </Col>
+      </Row>
+      <Row className=" w-100 text-center justify-content-center">
+        <Col sm={10} md={6} lg={6} xl={3}>
+          <Row className=" al-input-container justify-content-center py-4">
+            <Col className="al-form-group mb-3" sm={12}>
+              <TextInput icon={<AiOutlineUser />} />
+            </Col>
+            <Col className="al-form-group mb-3" sm={12}>
+              <TextInput icon={<RiLockPasswordFill />} />
+            </Col>
+            <Col className="al-form-group" sm={6}>
+              <Button className="al-lg-button"> Login </Button>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </Container>
+    // <div className="grid-container">
+    //   <form onSubmit={handleSubmit(onSubmit)}>
+    //     <input
+    //       className="border"
+    //       {...register('userName', { required: true })}
+    //     />
+    //     User
+    //     <hr />
+    //     <input
+    //       className="border"
+    //       {...register('password', { required: true })}
+    //     />
+    //     Password
+    //     <hr />
+    //     <Button type={'submit'}>Login</Button>
+    //     <hr />
+    //     {errors.exampleRequired && <span>This field is required</span>}
+    //   </form>
+    // </div>
   )
 }
 
--- src/app/shared/components/input/text-input/text-input.component.tsx
+++ src/app/shared/components/input/text-input/text-input.component.tsx
@@ -3,18 +3,20 @@ import { Props } from '../../../../model/root/root-model'
 import './text-input.component.scss'
 class TextInputProps extends Props {
   icon?: JSX.Element
+  label?: string
+  isPassword?: boolean
 }
 function TextInputComponent(props: TextInputProps) {
   return (
     <>
-      <div className="inputWrapper ">
-        {props.icon ? <i className="input-icon">{props.icon}</i> : <></>}
-        <input
-          className={`w-100 standart-input ${
-            props.icon ? 'input-with-icon' : ''
-          }`}
-          placeholder="Teste"
-        />
+      <div
+        className={`al-input-wrapper ${
+          !props.icon ? 'al-input-icon-disabled' : ''
+        }`}
+      >
+        <input className="al-input" type={'text'} {...props.input} />
+        {props.icon ? <i className="al-input-icon ">{props.icon}</i> : <></>}
+        {props.label ? <label>{props.label}</label> : <></>}
       </div>
     </>
   )
