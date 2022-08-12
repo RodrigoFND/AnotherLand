6d5cc2f91db055beb5e573d7d3c971f428459058
Rodrigo <rodrigofndev@gmail.com>
2022-08-12 07:25:34 -0300

    Ajustes para form

--- src/app/components/pages/login/login.component.tsx
+++ src/app/components/pages/login/login.component.tsx
@@ -4,72 +4,92 @@ import logo from '../../../assets/img/another-land-logo-with-name.png'
 import TextInput from '../../../shared/components/input/text-input/text-input.component'
 import { AiOutlineUser } from 'react-icons/ai'
 import { RiLockPasswordFill } from 'react-icons/ri'
-// import { FieldValues, useForm } from 'react-hook-form'
-// import { useAppDispatch } from '../../../store/hooks'
-// import { UserLogin } from '../../../model/auth/auth.models'
-// import { AuthAction } from '../../../store/auth-state/auth.reducer'
+import { FieldValues, useForm } from 'react-hook-form'
+import { useAppDispatch } from '../../../store/hooks'
+import { UserLogin } from '../../../model/auth/auth.models'
+import { AuthAction } from '../../../store/auth-state/auth.reducer'
 import { Button, Col, Container, Row } from 'react-bootstrap'
-function LoginComponent() {
-  // const dispatch = useAppDispatch()
-  // const {
-  //   register,
-  //   handleSubmit,
-  //   formState: { errors },
-  // } = useForm()
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
 
-  // const onSubmit = (data: FieldValues) => {
-  //   const userData = data as UserLogin
-  //   dispatch(AuthAction.loginWithPassword(userData))
-  // }
-  console.log('Login')
+const formSchema = yup.object().shape({
+  user: yup.string().required(),
+  password: yup.string().required(),
+})
+
+function LoginComponent() {
+  const dispatch = useAppDispatch()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(formSchema),
+  })
+
+  const onSubmit = (data: FieldValues) => {
+    const userData = data as UserLogin
+    console.log(data)
+    dispatch(AuthAction.loginWithPassword(userData))
+  }
+  console.log(errors)
   return (
-    <Container
-      fluid
-      className="login-view d-flex flex-column align-items-center justify-content-center al-login"
-    >
-      <Row className="w-100 text-center">
-        <Col sm={12}>
-          <picture>
-            <source srcSet={logo} />
-            <img src={logo} alt={'Another land logo'} className="logo" />
-          </picture>
-        </Col>
-      </Row>
-      <Row className=" w-100 text-center justify-content-center">
-        <Col sm={10} md={6} lg={6} xl={3}>
-          <Row className=" al-input-container justify-content-center py-4">
-            <Col className="al-form-group mb-3" sm={12}>
-              <TextInput icon={<AiOutlineUser />} />
-            </Col>
-            <Col className="al-form-group mb-3" sm={12}>
-              <TextInput icon={<RiLockPasswordFill />} />
-            </Col>
-            <Col className="al-form-group" sm={6}>
-              <Button className="al-lg-button"> Login </Button>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-    </Container>
-    // <div className="grid-container">
-    //   <form onSubmit={handleSubmit(onSubmit)}>
-    //     <input
-    //       className="border"
-    //       {...register('userName', { required: true })}
-    //     />
-    //     User
-    //     <hr />
-    //     <input
-    //       className="border"
-    //       {...register('password', { required: true })}
-    //     />
-    //     Password
-    //     <hr />
-    //     <Button type={'submit'}>Login</Button>
-    //     <hr />
-    //     {errors.exampleRequired && <span>This field is required</span>}
-    //   </form>
-    // </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Container
+        fluid
+        className="login-view d-flex flex-column align-items-center justify-content-center al-login"
+      >
+        <Row className="w-100 text-center">
+          <Col sm={12}>
+            <picture>
+              <source srcSet={logo} />
+              <img src={logo} alt={'Another land logo'} className="logo" />
+            </picture>
+          </Col>
+        </Row>
+
+        <Row className=" w-100 text-center justify-content-center">
+          <Col sm={10} md={6} lg={8} xl={4} xxl={3}>
+            <Row className=" al-input-container justify-content-center py-4">
+              <Col className="al-form-group mb-3" sm={12}>
+                <TextInput
+                  icon={<AiOutlineUser />}
+                  register={register}
+                  formName={'user'}
+                />
+              </Col>
+              <Col className="al-form-group mb-3" sm={12}>
+                <TextInput
+                  register={register}
+                  formName={'password'}
+                  icon={<RiLockPasswordFill />}
+                />
+              </Col>
+              <Col className="al-form-group" sm={8}>
+                <Button className="al-lg-button" type={'submit'}>
+                  Login
+                </Button>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+
+        {/* <div className="grid-container">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          User
+          <input className="border" {...register('user')} />
+          {errors.user?.message && <span>User is required</span>}
+          <hr />
+          Password
+          <input className="border" {...register('password')} />
+   
+          <hr />
+          <Button type={'submit'}>Login</Button>
+          <hr />
+        </form>
+      </div> */}
+      </Container>
+    </form>
   )
 }
 
--- src/app/shared/components/input/text-input/text-input.component.tsx
+++ src/app/shared/components/input/text-input/text-input.component.tsx
@@ -1,12 +1,32 @@
+import {
+  FieldValues,
+  Message,
+  MultipleFieldErrors,
+  Ref,
+  UseFormRegister,
+} from 'react-hook-form'
 import { Props } from '../../../../model/root/root-model'
-
 import './text-input.component.scss'
+
+export type FieldError = {
+  type: string
+  ref?: Ref
+  types?: MultipleFieldErrors
+  message?: Message
+}
+
 class TextInputProps extends Props {
   icon?: JSX.Element
   label?: string
   isPassword?: boolean
+  register?: UseFormRegister<FieldValues>
+  formName?: string
+  error?: FieldError
 }
+
 function TextInputComponent(props: TextInputProps) {
+  const register = props?.register ? props.register(props?.formName) : {}
+  const formError = props?.error.message
   return (
     <>
       <div
@@ -14,7 +34,12 @@ function TextInputComponent(props: TextInputProps) {
           !props.icon ? 'al-input-icon-disabled' : ''
         }`}
       >
-        <input className="al-input" type={'text'} {...props.input} />
+        <input className="al-input" type={'text'} {...register} />
+        {formError && (
+          <>
+            <span>{formError}</span>
+          </>
+        )}
         {props.icon ? <i className="al-input-icon ">{props.icon}</i> : <></>}
         {props.label ? <label>{props.label}</label> : <></>}
       </div>
