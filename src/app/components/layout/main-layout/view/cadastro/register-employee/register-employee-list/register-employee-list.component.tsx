a3740f8fd412cbc610fd44656b82baef79a099fb
RodrigoFND <rodrigofndev@gmail.com>
2022-08-19 13:39:48 -0300

    Feito ajustes na tabela Mui

--- src/app/components/layout/main-layout/view/cadastro/register-employee/register-employee-list/register-employee-list.component.tsx
+++ src/app/components/layout/main-layout/view/cadastro/register-employee/register-employee-list/register-employee-list.component.tsx
@@ -1,26 +1,19 @@
 import './register-employee-list.component.scss'
-import { useNavigate } from 'react-router-dom'
+// import { useNavigate } from 'react-router-dom'
 import { useAppSelector } from '../../../../../../../store/hooks'
 import { RegisterEmployee } from '../../../../../../../model/Register/register-employee/register-employee.models'
-import useRolePermission from '../../../../../../../shared/hooks/use-role-permission'
 import { Props } from '../../../../../../../model/root/root-model'
 import CustomBreadcrumbComponent from '../../../../../../../shared/components/breadcrumb/custom-breadcrumb.component'
-import { Button, Col, Container, Dropdown, Row } from 'react-bootstrap'
-import { DataGrid, GridColDef, GridRowParams } from '@mui/x-data-grid'
-import { RiLockPasswordFill } from 'react-icons/ri'
-Dropdown
+import { Button, Col, Container, Row } from 'react-bootstrap'
+import {
+  DataGrid,
+  GridActionsCellItem,
+  GridColumns,
+  GridRowParams,
+} from '@mui/x-data-grid'
 
-const EmployeeGrid = (employee: RegisterEmployee[]) => {
-  // const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-  //   event.stopPropagation()
-  //   setAnchorEl(event.currentTarget);
-  // };
-  // const handleClose = (e:React.MouseEvent<HTMLButtonElement, MouseEvent>,values) => {
-  //   e.stopPropagation()
-  //   console.log(values)
-  // };
-
-  const columns: GridColDef[] = [
+function EmployeeGrid(props: { employees: RegisterEmployee[] }) {
+  const columns: GridColumns<RegisterEmployee> = [
     {
       field: 'id',
       headerName: 'Id',
@@ -32,62 +25,30 @@ const EmployeeGrid = (employee: RegisterEmployee[]) => {
     { field: 'inactive', headerName: 'Inactive', flex: 0.3, type: 'boolean' },
     {
       field: 'actions',
+      type: 'actions',
       headerName: 'Actions',
-      sortable: false,
-      width: 200,
-      headerAlign: 'center',
-      align: 'center',
-      renderCell: () => {
-        // const onClick = (e: InputEvent) => {}
-        return (
-          <div className="al-dropdown">
-            <RiLockPasswordFill />
-            <div className="al-dropdown-content">Teste</div>
-
-            {/* <div
-            className="d-flex justify-content-between align-items-center"
-            style={{ cursor: "pointer" }}
-          >
-            <div onClick={handleClick} className='al-icon-click d-inline' 
-               id="fade-button"
-               aria-controls={open ? 'fade-menu' : undefined}
-               aria-haspopup="true"
-               aria-expanded={open ? 'true' : undefined}>
-                  <BsThreeDotsVertical size={20} 
-             />
-               </div>
-               <Menu
-        id="fade-menu"
-        MenuListProps={{
-          'aria-labelledby': 'fade-button',
-        }}
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        TransitionComponent={Fade}
-      >
-        <MenuItem onClick={handleClose}>Profile</MenuItem>
-        <MenuItem onClick={handleClose}>My account</MenuItem>
-        <MenuItem onClick={handleClose}>Logout</MenuItem>
-      </Menu>
-          </div> */}
-          </div>
-        )
-      },
+      getActions: (params: GridRowParams) => [
+        <GridActionsCellItem
+          key={0}
+          onClick={() => onRowClick(params)}
+          label="Delete"
+          showInMenu
+        />,
+      ],
     },
   ]
-
   const onRowClick = (params: GridRowParams) => {
     const rowData = params.row as RegisterEmployee
     console.log(rowData)
   }
+
   return (
     <>
       <DataGrid
         columns={columns}
-        rows={employee}
+        rows={props.employees}
         onRowClick={onRowClick}
-        getRowClassName={(params) => {
+        getRowClassName={() => {
           return 'al-register-employee-col'
         }}
         sx={{
@@ -100,19 +61,19 @@ const EmployeeGrid = (employee: RegisterEmployee[]) => {
 }
 
 function RegisterEmployeeListComponent(props: Props) {
-  const navigate = useNavigate()
+  // const navigate = useNavigate()
   const employees: RegisterEmployee[] = useAppSelector(
     (state) => state.registerEmployee.employees
   )
-  const rolesPermission = useRolePermission()
+  // const rolesPermission = useRolePermission()
 
-  const openEmployeeEditPage = (employeeId: number) => {
-    navigate(`../${employeeId}`, { replace: true })
-  }
+  // const openEmployeeEditPage = (employeeId: number) => {
+  //   navigate(`../${employeeId}`, { replace: true })
+  // }
 
-  const openAddPage = () => {
-    navigate(`../add`, { replace: true })
-  }
+  // const openAddPage = () => {
+  //   navigate(`../add`, { replace: true })
+  // }
   return (
     <>
       <CustomBreadcrumbComponent tree={props.tree} header={props.header}>
@@ -123,7 +84,7 @@ function RegisterEmployeeListComponent(props: Props) {
       <Container fluid className="al-form p-4">
         <Row>
           <Col className="al-register-employee-datagrid">
-            {EmployeeGrid(employees)}
+            <EmployeeGrid employees={employees} />
           </Col>
         </Row>
       </Container>
